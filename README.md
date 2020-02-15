9551e4d0d2bc441cead452eeeeeb3f50f3d3b3a9
Giannis Vasilopoulos <giannisvas123@gmail.com>
2020-02-15 17:03:25 +0200

    Set up React Context API

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 ## A simple hotel reservation site with React and Contentful for R&D. 
 
 A repo to research the [Contentful](https://www.contentful.com/)  as content delivery API. Possible integration of React Hooks and migrate the project for SSR.
+
+Also, i use React router for routing and React context API for state management.
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/Hero";
 import HeroBanner from "../components/HeroBanner";
 import Services from "../components/Services";
+import FeaturedRooms from "../components/FeaturedRooms";
 import { Link } from "react-router-dom";
 const Home = () => {
   return (
@@ -17,6 +18,7 @@ const Home = () => {
         </HeroBanner>
       </Hero>
       <Services />
+      <FeaturedRooms />
     </>
   );
 };
